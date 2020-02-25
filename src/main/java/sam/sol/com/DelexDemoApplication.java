c1f513eceb139d9138048df5881a7c0dbe13f785
Fiodar Motin <f.motin@sam-solutions.com>
2020-02-25 15:09:54 +0300

    created empty demo project

---
+++ src/main/java/sam/sol/com/DelexDemoApplication.java
@@ -0,0 +1,13 @@
+package sam.sol.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelexDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelexDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sam/sol/com/DelexDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.sol.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelexDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
